1f2febf8a485a6716d84b2a5f7aa5e1cf709c215
Anton Tymoshchuk <aatymoshchuk@gmail.com>
2020-10-01 18:37:43 +0300

    Add project files.

---
+++ ConsoleCallConsole/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleCallConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Diagnostics.Process process = new System.Diagnostics.Process();
+            System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+            startInfo.FileName = "cmd.exe";
+            startInfo.Arguments = "ping 127.0.0.1";
+            process.StartInfo = startInfo;
+            process.Start();
+        }
+    }
+}
